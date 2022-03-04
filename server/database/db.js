81ac1bffa19225b6074adee2066133847939d048
devesh <devehtripathi2020@gmail.com>
2022-03-04 17:43:32 +0530

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Connection = async() => {
+    const URL = `mongodb+srv://user:user@cluster0.ssfpb.mongodb.net/user?retryWrites=true&w=majority`;
+
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error);
+    }
+}
+
+export default Connection;
